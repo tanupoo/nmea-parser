30005fdd33c70b97cd2b5bdfb87fb3df0da6b0c9
Shoichi Sakane <github-shoichi@tanu.org>
2018-09-23 16:20:20 +0900

    changed options for more straightforward.

--- nmea-stat.py
+++ nmea-stat.py
@@ -10,7 +10,9 @@ import json
 from nmea_parser import nmea_parser
 
 pa = argparse.ArgumentParser(description="put a condition of GNSS.")
-pa.add_argument("-B", action="store_true", dest="break_mode",
+pa.add_argument("-c", action="store_true", dest="show_cond",
+               help="enables to show the condition of satelliates.")
+pa.add_argument("-s", action="store_true", dest="stream_mode",
                help="enables to show a statistics periodically. see -b option.")
 pa.add_argument("-b", action="store", dest="break_word", default="GGA",
                help="specify a word to break the stream input.")
@@ -31,6 +33,15 @@ def print_stat(result):
     print("Satellites in tracking: {}".format(len(result["tracked"])))
     print("Satellites in good signal: {}".format(len(result["n_good"])))
 
+def show_result():
+    if opt.show_cond:
+        result = nmea.eval(min_snr=32)
+        if opt.debug:
+            print(json.dumps(result,indent=4))
+        print_stat(result)
+    else:
+        print(json.dumps(nmea.get(),indent=4))
+
 '''
 main
 '''
@@ -38,11 +49,12 @@ nmea = nmea_parser()
 
 line_no = 0
 
-if opt.break_mode:
+if opt.stream_mode:
     for line in sys.stdin:
         line_no += 1
         if line[3:6] == opt.break_word:
-            print_stat(nmea.eval(min_snr=opt.min_snr))
+            #print_stat(nmea.eval(min_snr=opt.min_snr))
+            show_result()
             nmea.init()
         if nmea.append(line) == False:
             if opt.verbose:
@@ -51,20 +63,15 @@ if opt.break_mode:
     exit(0)
 
 #
-# non break_mode
+# non stream_mode
 #
 data = sys.stdin.read()
 
 for line in data.split("\n"):
     line_no += 1
     if nmea.append(line) == False:
-        if opt.debug or opt.verbose:
+        if opt.verbose:
             print("line {}: {}".format(line_no, nmea.strerror()))
 
-result = nmea.eval(min_snr=32)
-
-if opt.debug:
-    print(json.dumps(nmea.get(),indent=4))
-    print(json.dumps(result,indent=4))
-
-print_stat(result)
+# show result.
+show_result()
